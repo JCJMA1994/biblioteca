1e787f8874015ce8c4e087b5fe67220a5c299231
JCJMA1994 <201214026@uns.edu.pe>
2021-03-21 12:32:48 -0500

    Vistas y Modelos

--- biblioteca/applications/home/views.py
+++ biblioteca/applications/home/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import (
+    TemplateView,
+    ListView,
+)
+
 
 # Create your views here.
+class IndexView(TemplateView):
+    template_name = "home/index.html"
+
+class ListBook(ListView):
+    template_name = "home/list.html"
+    queryset = ['El quijote de la mancha', 'Cien años de soledad', 'Codigo Limpio']
+    context_object_name = "books"
--- biblioteca/applications/libreria/models.py
+++ biblioteca/applications/libreria/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField('Nombres', max_length=80)
+    nationality = models.CharField('Nacionalidad', blank=True, max_length=100)
+
+    def __str__(self):
+        return self.name
